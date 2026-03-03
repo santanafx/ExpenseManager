7fca8a75efaee9339c5343aa9e3582470eaf0060
lucasfbhs <lucasfigueiredo.bhs@fundep.com.br>
2026-03-03 13:49:49 -0300

    feat: create LoginInputModel class for user login input handling

---
+++ src/application/user/input-models/loginInputModel.ts
@@ -0,0 +1,9 @@
+export class LoginInputModel {
+  email: string
+  password: string
+
+  constructor(email: string, password: string) {
+    this.email = email;
+    this.password = password
+  }
+}
