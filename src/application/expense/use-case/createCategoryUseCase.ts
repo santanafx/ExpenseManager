fc404ca8ff8172e575a42e088e2b60e98412eb15
lucasfbhs <lucasfigueiredo.bhs@fundep.com.br>
2026-03-06 13:26:44 -0300

    refactor: correct import casing for CreateCategoryViewModel

--- src/application/expense/use-case/createCategoryUseCase.ts
+++ src/application/expense/use-case/createCategoryUseCase.ts
@@ -2,7 +2,7 @@ import { randomUUID } from "node:crypto";
 import { Category } from "../../../domain/expense/entities/category.js";
 import type { ICategoryRepository } from "../../../domain/expense/repositories/iCategoryRepository.js";
 import type { CreateCategoryInputModel } from "../input-models/createCategoryInputModel.js";
-import { CreateCategoryViewModel } from "../view-models/CreateCategoryViewModel.js";
+import { CreateCategoryViewModel } from "../view-models/createCategoryViewModel.js";
 
 export class CreateCategoryUseCase {
   constructor(private categoryRepository: ICategoryRepository) { }
