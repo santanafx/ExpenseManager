4523aa14039730ed75542b313ebeb5e284d6a87a
lucasfbhs <lucasfigueiredo.bhs@fundep.com.br>
2026-03-05 22:38:48 -0300

    refactor: simplify UserViewModel constructor by using readonly properties

--- src/application/user/view-models/userViewModel.ts
+++ src/application/user/view-models/userViewModel.ts
@@ -1,17 +1,10 @@
 import type { ROLE } from '../../../domain/user/enums/role.js'
 
 export class UserViewModel {
-  id: string
-  name: string
-  email: string
-  role: ROLE
-  created_at: Date
-
-  constructor(id: string, name: string, email: string, role: ROLE, created_at: Date) {
+  constructor(public readonly id: string, public readonly name: string, public readonly email: string, public readonly role: ROLE, public readonly created_at: Date) {
     this.id = id
     this.name = name
     this.email = email
     this.role = role
-    this.created_at = created_at
   }
 }
