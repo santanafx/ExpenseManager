0b80af24d4147244db0a82b5a19a7ab42e0edbdc
lucasfbhs <lucasfigueiredo.bhs@fundep.com.br>
2026-03-06 13:25:19 -0300

    refactor: remove CreateCategoryViewModel as part of API restructuring

--- src/application/expense/view-models/CreateCategoryViewModel.ts
+++
@@ -1,8 +0,0 @@
-export class CreateCategoryViewModel {
-  constructor(public readonly id: string, public readonly name: string, public readonly description: string, public readonly userId: string) {
-    this.id = id
-    this.description = description
-    this.name = name
-    this.userId = userId
-  }
-}
