d8d8f7e55d38ca40318a4c834de47e8c410d7538
lucasfbhs <lucasfigueiredo.bhs@fundep.com.br>
2026-03-05 22:38:26 -0300

    feat: implement CreateCategoryInputModel and CreateCategoryUseCase for category creation

---
+++ src/application/expense/use-case/createCategoryUseCase.ts
@@ -0,0 +1,25 @@
+import { randomUUID } from "node:crypto";
+import { Category } from "../../../domain/expense/entities/category.js";
+import type { ICategoryRepository } from "../../../domain/expense/repositories/iCategoryRepository.js";
+import type { CreateCategoryInputModel } from "../input-models/createCategoryInputModel.js";
+import { CreateCategoryViewModel } from "../view-models/CreateCategoryViewModel.js";
+
+export class CreateCategoryUseCase {
+  constructor(private categoryRepository: ICategoryRepository) { }
+
+  async execute(createCategoryInputModel: CreateCategoryInputModel) {
+    const isCategoryNameDuplicated = await this.categoryRepository.findByName(createCategoryInputModel.name)
+
+    if (isCategoryNameDuplicated) {
+      throw new Error('Category name already exists.')
+    }
+
+    const category = new Category(randomUUID(), createCategoryInputModel.name, createCategoryInputModel.description, createCategoryInputModel.userId)
+
+    const newCategory = await this.categoryRepository.create(category)
+
+    return new CreateCategoryViewModel(
+      newCategory.id, newCategory.description ?? "", newCategory.name, newCategory.userId
+    )
+  }
+}
