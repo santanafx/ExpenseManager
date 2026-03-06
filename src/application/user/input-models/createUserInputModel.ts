2ecffe133f01fb39a5eedee6dc839017ded58785
lucasfbhs <lucasfigueiredo.bhs@fundep.com.br>
2026-03-05 22:38:36 -0300

    refactor: simplify CreateUserInputModel constructor by using readonly properties

--- src/application/user/input-models/createUserInputModel.ts
+++ src/application/user/input-models/createUserInputModel.ts
@@ -1,12 +1,7 @@
 import type { ROLE } from '../../../domain/user/enums/role.js'
 
 export class CreateUserInputModel {
-  name: string
-  email: string
-  password: string
-  role?: ROLE | undefined
-
-  constructor(name: string, email: string, password: string, role?: ROLE | undefined) {
+  constructor(readonly name: string, readonly email: string, readonly password: string, readonly role?: ROLE | undefined) {
     this.name = name
     this.email = email
     this.password = password
