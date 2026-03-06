e74467cd80da1ffdfb88a738b10447584ba6eecb
lucasfbhs <lucasfigueiredo.bhs@fundep.com.br>
2026-03-06 13:26:07 -0300

    refactor: update findById method to findByDescription in IExpenseRepository

--- src/domain/expense/repositories/iExpenseRepository.ts
+++ src/domain/expense/repositories/iExpenseRepository.ts
@@ -2,5 +2,5 @@ import type { Expense } from "../entities/expense.js";
 
 export interface IExpenseRepository {
   create(expense: Expense): Promise<Expense>
-  findById(id: string): Promise<Expense | null>
+  findByDescription(description: string): Promise<Expense | null>
 }
