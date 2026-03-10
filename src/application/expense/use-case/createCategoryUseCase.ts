5648a619de7ae6f9c5db45d8bbe731242d2404cc
lucasfbhs <lucasfigueiredo.bhs@fundep.com.br>
2026-03-09 23:06:44 -0300

    refactor: update CreateCategoryUseCase to use object destructuring and improve type annotations

--- src/application/expense/use-case/createCategoryUseCase.ts
+++ src/application/expense/use-case/createCategoryUseCase.ts
@@ -2,24 +2,32 @@ import { randomUUID } from "node:crypto";
 import { Category } from "../../../domain/expense/entities/category.js";
 import type { ICategoryRepository } from "../../../domain/expense/repositories/iCategoryRepository.js";
 import type { CreateCategoryInputModel } from "../input-models/createCategoryInputModel.js";
-import { CreateCategoryViewModel } from "../view-models/createCategoryViewModel.js";
+import type { CreateCategoryViewModel } from "../view-models/createCategoryViewModel.js";
 
 export class CreateCategoryUseCase {
   constructor(private categoryRepository: ICategoryRepository) { }
 
-  async execute(createCategoryInputModel: CreateCategoryInputModel) {
+  async execute(createCategoryInputModel: CreateCategoryInputModel): Promise<CreateCategoryViewModel> {
     const isCategoryNameDuplicated = await this.categoryRepository.findByName(createCategoryInputModel.name)
 
     if (isCategoryNameDuplicated) {
       throw new Error('Category name already exists.')
     }
 
-    const category = new Category(randomUUID(), createCategoryInputModel.name, createCategoryInputModel.description, createCategoryInputModel.userId)
+    const category = new Category({
+      name: createCategoryInputModel.name,
+      ...(createCategoryInputModel.description !== undefined && { description: createCategoryInputModel.description }),
+      userId: createCategoryInputModel.userId,
+      created_at: new Date(),
+    }, randomUUID())
 
     const newCategory = await this.categoryRepository.create(category)
 
-    return new CreateCategoryViewModel(
-      newCategory.id, newCategory.description ?? "", newCategory.name, newCategory.userId
-    )
+    return {
+      id: newCategory.id,
+      name: newCategory.name,
+      description: newCategory.description ?? "",
+      userId: newCategory.userId,
+    }
   }
 }
