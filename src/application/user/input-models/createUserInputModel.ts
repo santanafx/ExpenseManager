8048ffe04c78dbb058ad2053eb43b0b186d5dcb5
lucasfbhs <lucasfigueiredo.bhs@fundep.com.br>
2026-03-09 23:07:29 -0300

    refactor: change CreateUserInputModel from class to interface

--- src/application/user/input-models/createUserInputModel.ts
+++ src/application/user/input-models/createUserInputModel.ts
@@ -1,10 +1,8 @@
 import type { ROLE } from '../../../domain/user/enums/role.js'
 
-export class CreateUserInputModel {
-  constructor(readonly name: string, readonly email: string, readonly password: string, readonly role?: ROLE | undefined) {
-    this.name = name
-    this.email = email
-    this.password = password
-    this.role = role
-  }
+export interface CreateUserInputModel {
+  name: string
+  email: string
+  password: string
+  role?: ROLE
 }
