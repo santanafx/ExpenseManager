1f23da61ec643b3fe6b9f58d2199ed78b386e124
lucasfbhs <lucasfigueiredo.bhs@fundep.com.br>
2026-03-09 23:07:35 -0300

    refactor: change LoginInputModel from class to interface

--- src/application/user/input-models/loginInputModel.ts
+++ src/application/user/input-models/loginInputModel.ts
@@ -1,6 +1,4 @@
-export class LoginInputModel {
-  constructor(readonly email: string, readonly password: string) {
-    this.email = email;
-    this.password = password
-  }
+export interface LoginInputModel {
+  email: string
+  password: string
 }
