78e21ca3a2a25417fd2538ad5f956cd4496cb0dc
lucasfbhs <lucasfigueiredo.bhs@fundep.com.br>
2026-03-09 23:08:53 -0300

    refactor: simplify Expense class by using ExpenseProps interface for constructor parameters

--- src/domain/expense/entities/expense.ts
+++ src/domain/expense/entities/expense.ts
@@ -1,83 +1,30 @@
 import { Entity } from "../../common/entity.js"
 import type { STATUS } from "../enums/status.js"
 
-export class Expense extends Entity {
-  constructor(
-    id: string,
-    private _description: string,
-    private _amount: number,
-    private _categoryId: string,
-    private _date: Date,
-    private _userId: string,
-    private _status: STATUS,
-    private _notes?: string,
-    private _created_at: Date = new Date(),
-    private _updated_at?: Date
-  ) {
-    super()
-    this.id = id
+interface ExpenseProps {
+  description: string
+  amount: number
+  categoryId: string
+  date: Date
+  userId: string
+  status: STATUS
+  notes?: string
+  created_at: Date
+  updated_at?: Date
+}
+
+export class Expense extends Entity<ExpenseProps> {
+  constructor(props: ExpenseProps, id: string) {
+    super(props, id)
   }
 
-  public get description(): string {
-    return this._description
-  }
-  private set description(value: string) {
-    this._description = value
-  }
-
-  public get amount(): number {
-    return this._amount
-  }
-  private set amount(value: number) {
-    this._amount = value
-  }
-
-  public get categoryId(): string {
-    return this._categoryId
-  }
-  public set categoryId(value: string) {
-    this._categoryId = value
-  }
-
-  public get date(): Date {
-    return this._date
-  }
-  private set date(value: Date) {
-    this._date = value
-  }
-
-  public get userId(): string {
-    return this._userId
-  }
-  private set userId(value: string) {
-    this._userId = value
-  }
-
-  public get status(): STATUS {
-    return this._status
-  }
-  private set status(value: STATUS) {
-    this._status = value
-  }
-
-  public get notes(): string | undefined {
-    return this._notes
-  }
-  private set notes(value: string) {
-    this._notes = value
-  }
-
-  public get created_at(): Date {
-    return this._created_at
-  }
-  private set created_at(value: Date) {
-    this._created_at = value
-  }
-
-  public get updated_at(): Date | undefined {
-    return this._updated_at
-  }
-  private set updated_at(value: Date) {
-    this._updated_at = value
-  }
+  get description(): string { return this.props.description }
+  get amount(): number { return this.props.amount }
+  get categoryId(): string { return this.props.categoryId }
+  get date(): Date { return this.props.date }
+  get userId(): string { return this.props.userId }
+  get status(): STATUS { return this.props.status }
+  get notes(): string | undefined { return this.props.notes }
+  get created_at(): Date { return this.props.created_at }
+  get updated_at(): Date | undefined { return this.props.updated_at }
 }
