d0cf759950d2f8bf9de0438495b8472b576d3699
lucasfbhs <lucasfigueiredo.bhs@fundep.com.br>
2026-03-09 23:06:34 -0300

    refactor: change CreateExpenseInputModel from class to interface

--- src/application/expense/input-models/createExpenseInputModel.ts
+++ src/application/expense/input-models/createExpenseInputModel.ts
@@ -1,13 +1,11 @@
 import type { STATUS } from "../../../domain/expense/enums/status.js"
 
-export class CreateExpenseInputModel {
-  constructor(public readonly description: string, public readonly amount: number, public readonly categoryId: string, public readonly date: Date, public readonly userId: string, public readonly status: STATUS, public readonly notes: string) {
-    this.description = description
-    this.amount = amount
-    this.categoryId = categoryId
-    this.date = date
-    this.userId = userId
-    this.status = status
-    this.notes = notes
-  }
+export interface CreateExpenseInputModel {
+  description: string
+  amount: number
+  categoryId: string
+  date: Date
+  userId: string
+  status: STATUS
+  notes?: string
 }
