16df4ad225c6b54ce85a12e8d57b53032f3919dd
lucasfbhs <lucasfigueiredo.bhs@fundep.com.br>
2026-03-09 23:08:13 -0300

    refactor: update Category class to use CategoryProps interface for constructor parameters

--- src/domain/expense/entities/category.ts
+++ src/domain/expense/entities/category.ts
@@ -1,53 +1,24 @@
-import { Entity } from "../../common/entity.js";
+import { Entity } from "../../common/entity.js"
 
-export class Category extends Entity {
-  constructor(
-    id: string,
-    private _name: string,
-    private _description?: string,
-    private _userId: string = "",
-    private _created_at: Date = new Date(),
-    private _updated_at?: Date
-  ) {
-    super()
-    this.id = id
-  }
+interface CategoryProps {
+  name: string
+  description?: string
+  userId: string
+  created_at: Date
+  updated_at?: Date
+}
 
-  public get name(): string {
-    return this._name
-  }
-  private set name(value: string) {
-    if (!value || value.trim().length === 0) {
+export class Category extends Entity<CategoryProps> {
+  constructor(props: CategoryProps, id: string) {
+    super(props, id)
+    if (!props.name || props.name.trim().length === 0) {
       throw new Error('Category name cannot be empty')
     }
-    this._name = value
   }
 
-  public get description(): string | undefined {
-    return this._description
-  }
-  private set description(value: string | undefined) {
-    this._description = value
-  }
-
-  public get userId(): string {
-    return this._userId
-  }
-  private set userId(value: string) {
-    this._userId = value
-  }
-
-  public get created_at(): Date {
-    return this._created_at
-  }
-  private set created_at(value: Date) {
-    this._created_at = value
-  }
-
-  public get updated_at(): Date | undefined {
-    return this._updated_at
-  }
-  private set updated_at(value: Date) {
-    this._updated_at = value
-  }
+  get name(): string { return this.props.name }
+  get description(): string | undefined { return this.props.description }
+  get userId(): string { return this.props.userId }
+  get created_at(): Date { return this.props.created_at }
+  get updated_at(): Date | undefined { return this.props.updated_at }
 }
