d951014d4ca6e1734fa8ff9ac455e9992269aa38
lucasfbhs <lucasfigueiredo.bhs@fundep.com.br>
2026-03-09 23:06:25 -0300

    refactor: change CreateCategoryInputModel from class to interface

--- src/application/expense/input-models/createCategoryInputModel.ts
+++ src/application/expense/input-models/createCategoryInputModel.ts
@@ -1,7 +1,5 @@
-export class CreateCategoryInputModel {
-  constructor(public readonly name: string, public readonly description: string, public readonly userId: string) {
-    this.name = name
-    this.description = description
-    this.userId = userId
-  }
+export interface CreateCategoryInputModel {
+  name: string
+  description?: string
+  userId: string
 }
