6211a890e7e375aee53bee93f25a3948a2ebd8c2
lucasfbhs <lucasfigueiredo.bhs@fundep.com.br>
2026-03-09 23:07:04 -0300

    refactor: change CreateExpepenseViewModel from class to interface

--- src/application/expense/view-models/createExpenseViewModel.ts
+++ src/application/expense/view-models/createExpenseViewModel.ts
@@ -1,14 +1,12 @@
 import type { STATUS } from "../../../domain/expense/enums/status.js"
 
-export class CreateExpepenseViewModel {
-  constructor(public readonly id: string, public readonly description: string, public readonly amount: number, public readonly categoryId: string, public readonly date: Date, public readonly userId: string, public readonly status: STATUS, public readonly notes: string) {
-    this.id = id
-    this.description = description
-    this.amount = amount
-    this.categoryId = categoryId
-    this.date = date
-    this.userId = userId
-    this.status = status
-    this.notes = notes
-  }
+export interface CreateExpepenseViewModel {
+  id: string
+  description: string
+  amount: number
+  categoryId: string
+  date: Date
+  userId: string
+  status: STATUS
+  notes: string
 }
