f838ffb796210f26b6286ed726b38912a539428a
lucasfbhs <lucasfigueiredo.bhs@fundep.com.br>
2026-03-09 23:08:06 -0300

    refactor: genericize Entity class to accept type parameters for props

--- src/domain/common/entity.ts
+++ src/domain/common/entity.ts
@@ -1,3 +1,9 @@
-export class Entity {
-  id!: string // gerado por uuid
+export class Entity<TProps> {
+  public readonly id: string
+  protected readonly props: TProps
+
+  protected constructor(props: TProps, id: string) {
+    this.id = id
+    this.props = props
+  }
 }
