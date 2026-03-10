5bfc11c05c71be4114240baec5b186f12a9ed4e0
lucasfbhs <lucasfigueiredo.bhs@fundep.com.br>
2026-03-09 23:07:59 -0300

    refactor: change UserViewModel from class to interface

--- src/application/user/view-models/userViewModel.ts
+++ src/application/user/view-models/userViewModel.ts
@@ -1,10 +1,9 @@
 import type { ROLE } from '../../../domain/user/enums/role.js'
 
-export class UserViewModel {
-  constructor(public readonly id: string, public readonly name: string, public readonly email: string, public readonly role: ROLE, public readonly created_at: Date) {
-    this.id = id
-    this.name = name
-    this.email = email
-    this.role = role
-  }
+export interface UserViewModel {
+  id: string
+  name: string
+  email: string
+  role: ROLE
+  created_at: Date
 }
