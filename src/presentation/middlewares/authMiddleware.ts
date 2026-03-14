716ed02ba3a8431133a00d5d0be1c5b5580579f4
lucasfbhs <lucasfigueiredo.bhs@fundep.com.br>
2026-03-14 10:15:35 -0300

    feat: import JWTService in authMiddleware and export authMiddlewareJwt instance

--- src/presentation/middlewares/authMiddleware.ts
+++ src/presentation/middlewares/authMiddleware.ts
@@ -1,5 +1,6 @@
 import type { Request, Response, NextFunction } from 'express'
 import type { IJWTService } from '../../application/common/interfaces/iJWTService.js'
+import { JWTService } from '../../infrastructure/services/jwtService.js'
 
 export function authMiddleware(jwtService: IJWTService) {
   return (req: Request, res: Response, next: NextFunction) => {
@@ -18,3 +19,6 @@ export function authMiddleware(jwtService: IJWTService) {
     }
   }
 }
+
+const jwtService = new JWTService()
+export const authMiddlewareJwt = authMiddleware(jwtService)
