fa192d93e767d6a674e9ec6e2dd58f2560445645
lucasfbhs <lucasfigueiredo.bhs@fundep.com.br>
2026-03-14 10:14:52 -0300

    feat: replace InvalidUsersCredentials with AppError for user authentication errors

--- src/application/user/use-case/autenticateUserUseCase.ts
+++ src/application/user/use-case/autenticateUserUseCase.ts
@@ -1,28 +1,27 @@
 import { compare } from "bcryptjs";
 import type { User } from "../../../domain/user/entities/user.js";
-import type { UserRepository } from "../../../infrastructure/persistence/user/repository/userRepository.js";
-import { InvalidUsersCredentials } from "../../common/errors/invalidUsersCredentials.js";
+import type { IUserRepository } from "../../../domain/user/repositories/iUserRepository.js";
+import { AppError } from "../../common/errors/appError.js";
 import type { IJWTService } from "../../common/interfaces/iJWTService.js";
-import { ROLE } from "../../../domain/user/enums/role.js";
 
 export class AutenticateUserUseCase {
   constructor(
-    private userRepository: UserRepository,
+    private userRepository: IUserRepository,
     private jwtService: IJWTService
   ) { }
 
   async execute(email: string, password: string): Promise<{ user: User; token: string }> {
     const user = await this.userRepository.findByEmail(email)
 
-    if (!user) throw new InvalidUsersCredentials()
+    if (!user) throw new AppError(401, 'Invalid user credentials.')
 
     const doesPasswordMatch = await compare(password, user.password_hash)
 
-    if (!doesPasswordMatch) throw new InvalidUsersCredentials()
+    if (!doesPasswordMatch) throw new AppError(401, 'Invalid user credentials.')
 
     const token = this.jwtService.sign({
       sub: user.id,
-      role: ROLE[user.role]
+      role: user.role
     })
 
     return {
