69a75ad776727bf46cffdbeecd9b75ba496a89ad
lucas <santanafx@hotmail.com>
2026-04-02 13:19:17 -0300

    feat: add Swagger UI setup for API documentation

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,14 @@ import express from 'express'
 
 import { errorMiddleware } from './presentation/middlewares/errorMiddleware.js'
 import { router } from './presentation/routes/index.js'
+import swaggerUi from 'swagger-ui-express'
+import { generateSwaggerDocument } from './presentation/docs/swaggerConfig.js'
 
 export const app = express()
 
 app.use(express.json())
 app.use('/api', router)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(generateSwaggerDocument()))
 app.use(errorMiddleware)
 
 const PORT = process.env.PORT || 3333
